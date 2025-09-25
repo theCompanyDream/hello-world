ee90e2b34e79bc9d95d02ce6589355cfa41f061c
Timothy Brantley II <tbrantleyii@gmail.com>
2025-09-25 14:59:11 -0400

    test: adding tests and actions (#2)
    * yay
    
    * naming
    
    * i have tests
    
    * I said what i said
    
    * ok now we have liftoff

--- hello/cli.py
+++ hello/cli.py
@@ -5,11 +5,15 @@ import os
 @click.argument('names', nargs=-1, type=click.Path())
 def cli(names):
     """Simple program that greets NAME for a total of COUNT times."""
+
+    if not names:
+        click.echo('No Arguments Provided')
+        return
+
     for name in names:
         if os.path.exists(name):
             with open(name, 'r') as file:
                 content = file.read().strip()
-                if content:
-                    click.echo(content)
+                click.echo(content)
         else:
             click.echo(f'Hello {name}!')

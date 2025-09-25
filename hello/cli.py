55ba4c05e2e68f40a4151940f37a223b4ef716bc
Timothy Brantley II <tbrantleyii@gmail.com>
2025-09-25 14:24:50 -0400

    fix: update click (#1)
    * fix: update click
    
    * fix: test cli
    
    * just wow

--- hello/cli.py
+++ hello/cli.py
@@ -1,10 +1,15 @@
 import click
+import os
 
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def cli(count, name):
+@click.argument('names', nargs=-1, type=click.Path())
+def cli(names):
     """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
+    for name in names:
+        if os.path.exists(name):
+            with open(name, 'r') as file:
+                content = file.read().strip()
+                if content:
+                    click.echo(content)
+        else:
+            click.echo(f'Hello {name}!')

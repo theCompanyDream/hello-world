edf41d19120be13afb2cdbfd7c462beb5064fbd9
companydream <tbrantleyii@gmail.com>
2025-09-25 14:24:18 -0400

    just wow

--- hello/cli.py
+++ hello/cli.py
@@ -1,7 +1,15 @@
 import click
+import os
 
 @click.command()
-@click.argument('name', nargs=-1, type=click.Path())
-def cli(name):
+@click.argument('names', nargs=-1, type=click.Path())
+def cli(names):
     """Simple program that greets NAME for a total of COUNT times."""
-    click.echo(f'Hello %s!' % ' '.join(name))
+    for name in names:
+        if os.path.exists(name):
+            with open(name, 'r') as file:
+                content = file.read().strip()
+                if content:
+                    click.echo(content)
+        else:
+            click.echo(f'Hello {name}!')
